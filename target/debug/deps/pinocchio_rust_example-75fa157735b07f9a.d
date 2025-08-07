2e267eb551969381bc2bbd718b6e9d7480877bf5
Bernou <jbmm@ugr.es>
2025-08-07 15:37:02 +0200

    Deletes target directory

--- target/debug/deps/pinocchio_rust_example-75fa157735b07f9a.d
+++
@@ -1,5 +0,0 @@
-/home/bernardo/Documents/pinnochio_rs/target/debug/deps/pinocchio_rust_example-75fa157735b07f9a: src/main.rs
-
-/home/bernardo/Documents/pinnochio_rs/target/debug/deps/pinocchio_rust_example-75fa157735b07f9a.d: src/main.rs
-
-src/main.rs:
